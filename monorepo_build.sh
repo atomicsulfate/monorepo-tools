57a338277a5f711a5b0e2cd388137e66202b1679
Andres Mandado <andres.mandado-almajano@here.com>
2022-03-29 19:09:47 +0200

    Merge all tags and branches

--- monorepo_build.sh
+++ monorepo_build.sh
@@ -1,13 +1,80 @@
 #!/usr/bin/env bash
 
 # Build monorepo from specified remotes
-# You must first add the remotes by "git remote add <remote-name> <repository-url>" and fetch from them by "git fetch --all"
-# Final monorepo will contain all branches from the first remote and master branches of all remotes will be merged
+# You must first add the remotes by "git remote add <remote-name> <repository-url>".
+# Final monorepo will contain all branches and tags from all remotes, merging those with the same
+# name.
 # If subdirectory is not specified remote name will be used instead
 #
 # Usage: monorepo_build.sh <remote-name>[:<subdirectory>] <remote-name>[:<subdirectory>] ...
 #
 # Example: monorepo_build.sh main-repository package-alpha:packages/alpha package-beta:packages/beta
+
+# Skip git filter-branch warning.
+export FILTER_BRANCH_SQUELCH_WARNING=1
+
+function remote_branches {
+	git branch -r --list $1/*| sed -e "s/[ \t]*$1\///"
+}
+
+function fetch_tags_from_remote {
+    REMOTE=$1
+    # Delete all local tags to avoid conflicts with tags from other remotes.
+    git tag -d $(git tag -l) > /dev/null
+
+    git fetch $REMOTE --tags
+}
+
+function get_remote_from_rev {
+    BRANCH=$(head -n 1 <<< $(git branch -r --contains $1))
+    echo ${BRANCH%%/*}
+}
+
+function document_merge {
+    REV_LINES=$(sed -e 's/\s\+/\n/g'  <<< "$1") # Split into one rev per line.
+    RESULT=""
+    # Prepend remote name to each rev.
+    for REV in $REV_LINES; do
+        RESULT+="$(get_remote_from_rev $REV) $REV"$'\n'
+    done
+    echo "$RESULT"
+}
+
+function merge_revs_in_current {
+    REF=$1
+    REVS=(${@:2})
+    REVS_STR="${REVS[@]}"
+    NUM_REVS=${#REVS[@]}
+    MERGE_DOC=`document_merge "$REVS_STR"`
+
+    echo -e "\t\t$REF, $NUM_REVS revs:\n$(sed 's/^/\t\t\t/' <<< $MERGE_DOC)"
+
+    # Just one revision, no need to merge.
+    if [[ $NUM_REVS -eq 1 ]]; then
+        return
+    fi
+
+    COMMIT_MSG="Merge $REF from all repos into monorepo. Revs:"$'\n'"$MERGE_DOC"
+    git merge --no-commit -q $REVS_STR --allow-unrelated-histories > /dev/null
+    # Resolving conflicts using trees of all parents
+    for REV in $REVS_STR; do
+        # Add all files from all master branches into index
+        # "git read-tree" with multiple refs cannot be used as it is limited to 8 refs
+        git ls-tree -r $REV | git update-index --index-info  > /dev/null
+    done
+    git commit -m "$COMMIT_MSG" > /dev/null
+    git reset --hard  > /dev/null
+}
+
+function cleanup-local {
+    # Checking out orphan commit so it 's possible to delete current branch
+    git checkout -q --orphan void
+    git reset --hard > /dev/null
+    git clean -fdx > /dev/null
+    for BRANCH in $(git branch); do
+        git branch -D $BRANCH > /dev/null
+    done
+}
 
 # Check provided arguments
 if [ "$#" -lt "2" ]; then
@@ -19,7 +86,15 @@ fi
 # Get directory of the other scripts
 MONOREPO_SCRIPT_DIR=$(dirname "$0")
 # Wipe original refs (possible left-over back-up after rewriting git history)
-$MONOREPO_SCRIPT_DIR/original_refs_wipe.sh
+$MONOREPO_SCRIPT_DIR/original_refs_wipe.sh > /dev/null
+
+declare -A MERGE_BRANCHES
+declare -A MERGE_TAGS
+
+cleanup-local > /dev/null
+
+echo "1. Rewrite history for all refs (branch and tags) across all remotes"
+
 for PARAM in $@; do
     # Parse parameters in format <remote-name>[:<subdirectory>]
     PARAM_ARR=(${PARAM//:/ })
@@ -28,32 +103,55 @@ for PARAM in $@; do
     if [ "$SUBDIRECTORY" == "" ]; then
         SUBDIRECTORY=$REMOTE
     fi
-    # Rewrite all branches from the first remote, only master branches from others
-    if [ "$PARAM" == "$1" ]; then
-        echo "Building all branches of the remote '$REMOTE'"
-        $MONOREPO_SCRIPT_DIR/load_branches_from_remote.sh $REMOTE
-        $MONOREPO_SCRIPT_DIR/rewrite_history_into.sh $SUBDIRECTORY --branches
-        MERGE_REFS='master'
-    else
-        echo "Building branch 'master' of the remote '$REMOTE'"
-        git checkout --detach $REMOTE/master
-        $MONOREPO_SCRIPT_DIR/rewrite_history_into.sh $SUBDIRECTORY
-        MERGE_REFS="$MERGE_REFS $(git rev-parse HEAD)"
-    fi
+    echo -e "\tRemote '$REMOTE'"
+    echo -e "\t\tFetch all tags"
+    fetch_tags_from_remote $REMOTE
+    echo -e "\t\tRewrite history to move files into subdir '$SUBDIRECTORY'"
+    # git filter-branch needs some valid HEAD.
+    git checkout -q -B master $(git rev-parse $REMOTE/master)
+    $MONOREPO_SCRIPT_DIR/rewrite_history_into.sh $SUBDIRECTORY --remotes=$REMOTE --tags
+    cleanup-local > /dev/null
+
+    for BRANCH in $(remote_branches $REMOTE); do
+        MERGE_BRANCHES[$BRANCH]="${MERGE_BRANCHES[$BRANCH]} $(git rev-parse $REMOTE/$BRANCH)"
+    done
+
+    for TAG in $(git tag -l); do
+        MERGE_TAGS[$TAG]="${MERGE_TAGS[$TAG]} $(git rev-parse $TAG)"
+        git tag -d $TAG > /dev/null # clean up to avoid conflicts with tags from next remote.
+    done
     # Wipe the back-up of original history
-    $MONOREPO_SCRIPT_DIR/original_refs_wipe.sh
+    $MONOREPO_SCRIPT_DIR/original_refs_wipe.sh > /dev/null
 done
-# Merge all master branches
-COMMIT_MSG="merge multiple repositories into a monorepo"$'\n'$'\n'"- merged using: 'monorepo_build.sh $@'"$'\n'"- see https://github.com/shopsys/monorepo-tools"
-git checkout master
-echo "Merging refs: $MERGE_REFS"
-git merge --no-commit -q $MERGE_REFS --allow-unrelated-histories
-echo 'Resolving conflicts using trees of all parents'
-for REF in $MERGE_REFS; do
-    # Add all files from all master branches into index
-    # "git read-tree" with multiple refs cannot be used as it is limited to 8 refs
-    git ls-tree -r $REF | git update-index --index-info
+
+echo "2. Merge branches and tags with same names across remotes"
+
+echo -e "\tMerge Branches"
+for BRANCH in "${!MERGE_BRANCHES[@]}"; do
+    REVS=(${MERGE_BRANCHES[$BRANCH]})
+    FIRST_REV=${REVS[0]}
+    git checkout -q -b $BRANCH $FIRST_REV
+
+    merge_revs_in_current $BRANCH ${REVS[@]}
 done
-git commit -m "$COMMIT_MSG"
-git reset --hard
+
+echo -e "\tMerge Tags"
+for TAG in "${!MERGE_TAGS[@]}"; do
+    REVS=(${MERGE_TAGS[$TAG]})
+    FIRST_REV=${REVS[0]}
+
+    # Create a temporal branch to do the merge.
+    TMP_BRANCH="${TAG}_tmpBranch"
+    git checkout -q -b $TMP_BRANCH $FIRST_REV
+
+    merge_revs_in_current $TAG ${REVS[@]}
+
+    # Create the tag from the tmp branch and delete branch.
+    git tag -f $TAG > /dev/null
+    git checkout -q $TAG
+    git branch -q -D $TMP_BRANCH
+done
+
+echo "3. Review created branches and tags. If all's well, push with 'git push -f <monorepo_remote>'"
+
 
--- rewrite_history_into.sh
+++ rewrite_history_into.sh
@@ -11,7 +11,6 @@
 
 SUBDIRECTORY=$1
 REV_LIST_PARAMS=${@:2}
-echo "Rewriting history into a subdirectory '$SUBDIRECTORY'"
 # All paths in the index are prefixed with a subdirectory and the index is updated
 # Previous index file is replaced by a new one (otherwise each file would be in the index twice)
 # The tags are rewritten as well as commits (the "cat" command will use original name without any change)
